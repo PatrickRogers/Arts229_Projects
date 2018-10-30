2b7e5bd55963c72eb5415e3fbb431813b7e6203d
PatrickRogers <42785056+PatrickRogers@users.noreply.github.com>
2018-10-30 02:28:09 -0400

    Update project1.js

--- Projects/project1.js
+++ Projects/project1.js
@@ -8,3 +8,10 @@ btn.addEventListener('mouseenter', () => {
 btn.addEventListener('mouseleave', () => {
 	body.classList.remove('show');
 });
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
